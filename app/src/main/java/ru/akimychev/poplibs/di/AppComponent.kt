83907785aa57084bd6b5b9fb3061e80e969cf628
Maksim Akimychev <101060239+akimychau@users.noreply.github.com>
2023-05-13 16:15:52 +0200

    Урок 6 начали | AppComponent for Dagger (deleted useless injections)

--- app/src/main/java/ru/akimychev/poplibs/di/AppComponent.kt
+++ app/src/main/java/ru/akimychev/poplibs/di/AppComponent.kt
@@ -1,12 +1,9 @@
 package ru.akimychev.poplibs.di
 
 import dagger.Component
-import ru.akimychev.poplibs.details.GithubUserReposFragment
 import ru.akimychev.poplibs.details.GithubUserReposPresenter
 import ru.akimychev.poplibs.di.modules.*
-import ru.akimychev.poplibs.forksCount.ForksCountFragment
 import ru.akimychev.poplibs.forksCount.ForksCountPresenter
-import ru.akimychev.poplibs.list.GithubUsersFragment
 import ru.akimychev.poplibs.list.GithubUsersPresenter
 import ru.akimychev.poplibs.main.MainActivity
 import ru.akimychev.poplibs.main.MainPresenter
@@ -22,16 +19,10 @@ import javax.inject.Singleton
         RepositoriesModule::class
     ]
 )
-interface AppComponent{
+interface AppComponent {
     fun inject(mainActivity: MainActivity)
     fun inject(mainPresenter: MainPresenter)
-
     fun inject(githubUsersPresenter: GithubUsersPresenter)
-    fun inject(githubUsersFragment: GithubUsersFragment)
-
     fun inject(githubUserReposPresenter: GithubUserReposPresenter)
-    fun inject(githubUserReposFragment: GithubUserReposFragment)
-
     fun inject(forksCountPresenter: ForksCountPresenter)
-    fun inject(forksCountFragment: ForksCountFragment)
 }
